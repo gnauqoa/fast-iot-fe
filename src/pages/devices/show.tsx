e9ab3102a9d6211d1cf2a5cd73c49a8ac9b12db1
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2025-04-03 17:31:11 +0700

    refactor: device show page

--- src/pages/devices/show.tsx
+++ src/pages/devices/show.tsx
@@ -1,21 +1,18 @@
 import { Link, useShow } from "@refinedev/core";
 import { Show } from "@refinedev/antd";
-import { Badge, Button, Col, Flex, message, Row, Typography } from "antd";
+import { Badge, Button, Flex, message, Typography } from "antd";
 import { DeviceStatus, IDevice } from "../../interfaces/device";
 import { useEffect, useState } from "react";
-import { Icon } from "@iconify/react";
 import { socket } from "../../providers/liveProvider";
 import { capitalize } from "../../utility/text";
-import { SensorCard, SliderCard, SwitchCard } from "../../components/card";
 import {
   HANDLE_DEVICE_DATA_CHANNEL,
   JOIN_DEVICE_ROOM_CHANNEL,
   LEAVE_DEVICE_ROOM_CHANNEL,
-  UPDATE_DEVICE_CHANNEL,
 } from "../../constants";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { EyeOutlined, ReloadOutlined } from "@ant-design/icons"; // Import icons từ Ant Design
+import { EyeOutlined, ReloadOutlined } from "@ant-design/icons";
 import { axiosInstance } from "@refinedev/nestjsx-crud";
 
 dayjs.extend(relativeTime);
@@ -34,7 +31,7 @@ export const DeviceShow = () => {
       const response = await axiosInstance.get(
         `/devices/${device.id}/password`
       );
-      setDeviceToken(response.data.device_pass);
+      setDeviceToken(`${response.data.deviceKey}-${response.data.deviceToken}`);
       message.success("Device token created!");
     } catch (error) {
       message.error("Failed to create device token.");
@@ -42,6 +39,7 @@ export const DeviceShow = () => {
   };
 
   const handleDeviceUpdate = (updatedDevice: IDevice) => {
+    console.log({ updatedDevice })
     setDevice(updatedDevice);
   };
 
@@ -112,184 +110,10 @@ export const DeviceShow = () => {
         </Flex>
 
         <Flex>
-          <Link to={`/users/${device.user_id}`}>
+          <Link to={`/users/${device.userId}`}>
             <Title level={5}>{device.user.fullName}</Title>
           </Link>
         </Flex>
-        <Row gutter={[16, 16]}>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <SensorCard
-              icon={
-                <Icon
-                  icon="material-symbols-light:humidity-percentage-outline"
-                  width="32"
-                  height="32"
-                />
-              }
-              title="Humidity"
-              value={device.humi}
-              uint="%"
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <SensorCard
-              icon={<Icon icon="circum:temp-high" width="32" height="32 " />}
-              title="Temperature"
-              value={device.temp}
-              uint="°C"
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <SensorCard
-              icon={
-                <Icon
-                  icon="material-symbols-light:water-lux-outline"
-                  width="32"
-                  height="32"
-                />
-              }
-              title="Light"
-              value={device.lux}
-              uint="lux"
-            />
-          </Col>
-        </Row>
-        <Row gutter={[16, 16]}>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <SwitchCard
-              title="Button 1"
-              value={device.btn1}
-              onChange={(checked) => {
-                socket.emit(UPDATE_DEVICE_CHANNEL, {
-                  ...device,
-                  btn1: checked,
-                } as IDevice);
-              }}
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <SwitchCard
-              title="Button 2"
-              value={device.btn2}
-              onChange={(checked) => {
-                socket.emit(UPDATE_DEVICE_CHANNEL, {
-                  ...device,
-                  btn2: checked,
-                } as IDevice);
-              }}
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <SwitchCard
-              title="Light"
-              value={device.btn3}
-              onChange={(checked) => {
-                socket.emit(UPDATE_DEVICE_CHANNEL, {
-                  ...device,
-                  btn3: checked,
-                } as IDevice);
-              }}
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <SwitchCard
-              title="Pump"
-              value={device.btn4}
-              onChange={(checked) => {
-                socket.emit(UPDATE_DEVICE_CHANNEL, {
-                  ...device,
-                  btn4: checked,
-                } as IDevice);
-              }}
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <SwitchCard
-              title="Auto control"
-              value={device.autoControl}
-              onChange={(checked) => {
-                socket.emit(UPDATE_DEVICE_CHANNEL, {
-                  ...device,
-                  autoControl: checked,
-                } as IDevice);
-              }}
-            />
-          </Col>
-        </Row>
-        <Row gutter={[16, 16]}>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <SliderCard
-              title="Temp Range"
-              value={device.tempRange || 0}
-              min={0}
-              max={100}
-              step={1}
-              onChange={(value) => {
-                setDevice({ ...device, tempRange: value });
-              }}
-              onFinalChange={(value) => {
-                socket.emit(UPDATE_DEVICE_CHANNEL, {
-                  ...device,
-                  tempRange: value,
-                });
-              }}
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <SliderCard
-              title="Humi Range"
-              value={device.humiRange || 0}
-              min={0}
-              max={100}
-              step={1}
-              onChange={(value) => {
-                setDevice({ ...device, humiRange: value });
-              }}
-              onFinalChange={(value) => {
-                socket.emit(UPDATE_DEVICE_CHANNEL, {
-                  ...device,
-                  humiRange: value,
-                });
-              }}
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <SliderCard
-              title="Lux Range"
-              value={device.luxRange || 0}
-              min={0}
-              max={100}
-              step={1}
-              onChange={(value) => {
-                setDevice({ ...device, luxRange: value });
-              }}
-              onFinalChange={(value) => {
-                socket.emit(UPDATE_DEVICE_CHANNEL, {
-                  ...device,
-                  luxRange: value,
-                });
-              }}
-            />
-          </Col>
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <SliderCard
-              title="Mosfet Speed"
-              value={device.mosfetSpeed || 0}
-              min={0}
-              max={100}
-              step={1}
-              onChange={(value) => {
-                setDevice({ ...device, mosfetSpeed: value });
-              }}
-              onFinalChange={(value) => {
-                socket.emit(UPDATE_DEVICE_CHANNEL, {
-                  ...device,
-                  mosfetSpeed: value,
-                });
-              }}
-            />
-          </Col>
-        </Row>
       </Flex>
     </Show>
   );
