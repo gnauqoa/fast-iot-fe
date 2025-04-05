5592738b5bc0c9b4c6ca9d1cc9c43009e99ab847
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2025-04-05 15:23:04 +0700

    feat: remove filter user by created at

--- src/pages/users/list.tsx
+++ src/pages/users/list.tsx
@@ -6,37 +6,23 @@ import {
   DateField,
   getDefaultSortOrder,
   DeleteButton,
-} from "@refinedev/antd";
-import { SearchOutlined } from "@ant-design/icons";
-import {
-  Table,
-  Space,
-  Form,
-  Input,
-  DatePicker,
-  Button,
-  Tag,
-  Select,
-} from "antd";
-import { IUser, UserStatus, UserRole } from "../../interfaces/user";
-import {
-  CrudFilters,
-  HttpError,
-  LogicalFilter,
-  useSubscription,
-} from "@refinedev/core";
-import dayjs from "dayjs";
-import { UserCreateModal } from "../../components/users";
+} from '@refinedev/antd';
+import { SearchOutlined } from '@ant-design/icons';
+import { Table, Space, Form, Input, DatePicker, Button, Tag, Select } from 'antd';
+import { IUser, UserStatus, UserRole } from '../../interfaces/user';
+import { CrudFilters, HttpError, LogicalFilter, useSubscription } from '@refinedev/core';
+import dayjs from 'dayjs';
+import { UserCreateModal } from '../../components/users';
 
 const { RangePicker } = DatePicker;
 const roleColors: Record<UserRole, string> = {
-  [UserRole.ADMIN]: "gold",
-  [UserRole.USER]: "blue",
+  [UserRole.ADMIN]: 'gold',
+  [UserRole.USER]: 'blue',
 };
 
 const statusColors: Record<UserStatus, string> = {
-  [UserStatus.ACTIVE]: "green",
-  [UserStatus.INACTIVE]: "red",
+  [UserStatus.ACTIVE]: 'green',
+  [UserStatus.INACTIVE]: 'red',
 };
 
 export const UserList = () => {
@@ -45,58 +31,37 @@ export const UserList = () => {
     HttpError,
     {
       fullName: string;
-      createdAt: [string, string]; // Dữ liệu từ URL có thể là string
       role: string;
       status: string;
     }
   >({
-    onSearch: (params) => {
+    onSearch: params => {
       const filters: CrudFilters = [];
-      const { fullName, createdAt, role, status } = params as any;
+      const { fullName, role, status } = params as any;
 
       if (fullName) {
         filters.push({
-          field: "fullName",
-          operator: "contains",
+          field: 'fullName',
+          operator: 'contains',
           value: fullName,
         });
       }
 
-      if (createdAt && Array.isArray(createdAt) && createdAt.length === 2) {
-        const startDate = dayjs(createdAt[0]);
-        const endDate = dayjs(createdAt[1]);
-
-        if (startDate.isValid() && endDate.isValid()) {
-          filters.push(
-            {
-              field: "createdAt",
-              operator: "gte",
-              value: startDate.toISOString(),
-            },
-            {
-              field: "createdAt",
-              operator: "lte",
-              value: endDate.toISOString(),
-            }
-          );
-        }
-      }
-
       if (role) {
-        filters.push({ field: "role.name", operator: "eq", value: role });
+        filters.push({ field: 'role.name', operator: 'eq', value: role });
       }
 
       if (status) {
-        filters.push({ field: "status.name", operator: "eq", value: status });
+        filters.push({ field: 'status.name', operator: 'eq', value: status });
       }
 
       return filters;
     },
-    defaultSetFilterBehavior: "replace",
+    defaultSetFilterBehavior: 'replace',
   });
   useSubscription({
-    channel: "messages",
-    onLiveEvent: (event) => {
+    channel: 'messages',
+    onLiveEvent: event => {
       console.log({
         message: `Tin nhắn mới: ${event.payload.text}`,
       });
@@ -105,28 +70,9 @@ export const UserList = () => {
 
   // Chuyển filters thành initialValues
   const initialValues = {
-    fullName:
-      (filters as LogicalFilter[])?.find((f) => f.field === "fullName")
-        ?.value || "",
-    role:
-      (filters as LogicalFilter[])?.find((f) => f.field === "role.name")
-        ?.value || undefined,
-    status:
-      (filters as LogicalFilter[])?.find((f) => f.field === "status.name")
-        ?.value || undefined,
-    createdAt: (() => {
-      const gte = (filters as LogicalFilter[])?.find(
-        (f) => f.field === "createdAt" && f.operator === "gte"
-      )?.value;
-      const lte = (filters as LogicalFilter[])?.find(
-        (f) => f.field === "createdAt" && f.operator === "lte"
-      )?.value;
-
-      if (gte && lte) {
-        return [dayjs(gte), dayjs(lte)];
-      }
-      return [];
-    })(),
+    fullName: (filters as LogicalFilter[])?.find(f => f.field === 'fullName')?.value || '',
+    role: (filters as LogicalFilter[])?.find(f => f.field === 'role.name')?.value || undefined,
+    status: (filters as LogicalFilter[])?.find(f => f.field === 'status.name')?.value || undefined,
   };
 
   const handleClearFilters = () => {
@@ -134,7 +80,6 @@ export const UserList = () => {
       fullName: undefined,
       role: undefined,
       status: undefined,
-      createdAt: undefined,
     });
     searchFormProps.form?.submit();
   };
@@ -155,7 +100,7 @@ export const UserList = () => {
             placeholder="Select Role"
             allowClear
             style={{ width: 150 }}
-            options={Object.values(UserRole).map((role) => ({
+            options={Object.values(UserRole).map(role => ({
               label: (
                 <Tag color={roleColors[role]} style={{ margin: 0 }}>
                   {role}
@@ -170,7 +115,7 @@ export const UserList = () => {
             placeholder="Select Status"
             allowClear
             style={{ width: 150 }}
-            options={Object.values(UserStatus).map((status) => ({
+            options={Object.values(UserStatus).map(status => ({
               label: (
                 <Tag color={statusColors[status]} style={{ margin: 0 }}>
                   {status}
@@ -180,11 +125,6 @@ export const UserList = () => {
             }))}
           />
         </Form.Item>
-        {initialValues.createdAt && (
-          <Form.Item name="createdAt" initialValue={initialValues.createdAt}>
-            <RangePicker />
-          </Form.Item>
-        )}
         <Form.Item>
           <Button type="primary" htmlType="submit">
             Filter
@@ -202,25 +142,21 @@ export const UserList = () => {
         <Table.Column dataIndex="email" title="Email" />
         <Table.Column dataIndex="fullName" title="Full Name" />
         <Table.Column
-          dataIndex={["role", "name"]}
+          dataIndex={['role', 'name']}
           title="Role"
-          render={(role: UserRole) => (
-            <Tag color={roleColors[role]}>{role}</Tag>
-          )}
+          render={(role: UserRole) => <Tag color={roleColors[role]}>{role}</Tag>}
         />
         <Table.Column
-          dataIndex={["status", "name"]}
+          dataIndex={['status', 'name']}
           title="Status"
-          render={(status: UserStatus) => (
-            <Tag color={statusColors[status]}>{status}</Tag>
-          )}
+          render={(status: UserStatus) => <Tag color={statusColors[status]}>{status}</Tag>}
         />
         <Table.Column
           dataIndex="createdAt"
           title="Created At"
           sorter
-          defaultSortOrder={getDefaultSortOrder("createdAt", sorter)}
-          render={(value) => <DateField value={value} format="LLL" />}
+          defaultSortOrder={getDefaultSortOrder('createdAt', sorter)}
+          render={value => <DateField value={value} format="LLL" />}
         />
         <Table.Column
           title="Actions"
